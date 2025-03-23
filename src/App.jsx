e5edf2645e90c2b3d202e0533e15ed01ec70fe2a
Ameer Hamza <Ameerhamza4207@gmail.com>
2025-03-23 16:00:58 +0500

    code pushIn

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+import Card from './components/Card'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Header/>
+    <div >
+    <h1 className=" text-4xl font-bold text-center text-blue-600 my-8  decoration-2 decoration-blue-500">
+          List of Cards
+        </h1>
+      <div className='container mx-auto px-4 py-8 gap-2 flex flex-wrap justify-center'>
+    <Card/>
+    <Card/>
+    <Card/>
+
+      </div>
+
+
+    </div>
+    </>
+  )
+}
+
+export default App

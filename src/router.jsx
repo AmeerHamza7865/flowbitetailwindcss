ce263627e3c4db7fa783f4708293901cd8fe8aee
Ameer Hamza <Ameerhamza4207.com>
2025-04-14 17:32:00 +0500

    update router

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,13 @@
 // src/Router.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 //  import Product from './pages/Product';
 import Product from './pages/Product';
 import NotFound from './pages/NotFound';
+import Home from './Pages/Home';
 
 const AppRouter = () => {
   return (
